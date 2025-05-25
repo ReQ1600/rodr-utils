aac76d20860fe2f44a4f31e13a6ada090da8da7b
Mateusz Drogowski <70808720+ReQ1600@users.noreply.github.com>
2025-05-26 00:27:27 +0200

    Update rodrUtils.hpp

--- rodrUtils.hpp
+++ rodrUtils.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <functional>
+#include <map>
 
 namespace rodr
 {
@@ -9,27 +10,32 @@ namespace rodr
     {
         Creation = 0,
         Receive,
-        AcceptPos,
+        BadReturnVal,
+        StmAcceptPos,
         SendCmd,
-        SendPos
+        SendPos,
+        OpenFile
     };
 
     namespace err_src
     {
         constexpr const char* TCP = "TCP";
         constexpr const char* UDP = "UDP";
-    };
-
+        constexpr const char* RECORDING = "RECORDING";
+    }
 
     namespace err_msgs
     {
         constexpr const char* CREATION = "Socket creation failed";
 
         constexpr const char* RECEIVE = "No data received";
-        constexpr const char* ACCEPTPOS = "Bad return value";
+        constexpr const char* BADRETURNVAL = "Bad return value";
+        constexpr const char* STMACCEPTPOS = "Bad position argument";
 
         constexpr const char* SENDCMD = "Could not send command";
         constexpr const char* SENDPOS = "Could not send position";
+
+        constexpr const char* OPEN = "Could not open file at the specified path";
 
         constexpr const char* getErrorMsg(ERROR_TYPE type)
         {
@@ -37,11 +43,21 @@ namespace rodr
             {
                 case ERROR_TYPE::Creation: return CREATION;
                 case ERROR_TYPE::Receive: return RECEIVE;
-                case ERROR_TYPE::AcceptPos: return ACCEPTPOS;
+                case ERROR_TYPE::BadReturnVal: return BADRETURNVAL;
+                case ERROR_TYPE::StmAcceptPos: return STMACCEPTPOS;
                 case ERROR_TYPE::SendCmd: return SENDCMD;
                 case ERROR_TYPE::SendPos: return SENDPOS;
+                case ERROR_TYPE::OpenFile: return OPEN;
                 default: return "bad type";
             }
         }
+    }
+
+    //map for filtering feedback columns
+    const std::map<const char*, int> ColmumnMap = {
+        {"time", 0},
+        {"pos", 1},
+        {"hum", 2}
     };
+
 }
